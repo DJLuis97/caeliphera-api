24d5b63aa80d5fe04321c450a5b68eb38577b578
Luis Suárez <xboxluis2010@hotmail.com>
2022-06-29 21:00:43 -0500

    reformat code

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
@@ -7,7 +6,6 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-class Controller extends BaseController
-{
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+class Controller extends BaseController {
+	use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
 }
