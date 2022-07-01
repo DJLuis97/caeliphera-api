de9230c41eda645fdab143e7eba681270eaf9ff0
Luis Suárez <xboxluis2010@hotmail.com>
2022-06-30 19:51:51 -0500

    Refactorizar login
    * Ahora se guarda una referencia del nombre del dispositivo
    
    * Se validan las credenciales proporcionadas

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -2,25 +2,35 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\StoreRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthenticatedSessionController extends Controller {
-	public function store (Request $request) {
-		if (Auth::attempt($request->only(['email', 'password']))) {
-			$user = User::query()->where('email', $request['email'])->firstOrFail();
-			$token = $user->createToken('auth_token')->plainTextToken;
-
-			return (new UserResource($user))->additional([
-				'access_token' => $token,
-				'token_type'   => 'Bearer'
+	/**
+	 * @param StoreRequest $request
+	 *
+	 * @return UserResource
+	 * @throws ValidationException
+	 */
+	public function store (StoreRequest $request): UserResource {
+		$validated = $request->validated();
+		$user = User::query()->with('person')->where('email', $validated['email'])->first();
+		if ( ! $user or ! Hash::check($validated['password'], $user->password)) {
+			throw ValidationException::withMessages([
+				'email' => 'Las credenciales proporcionadas son incorrectas.'
 			]);
 		}
 
-		return response()->json('Acceso no autorizado.', 401);
+		return (new UserResource($user))->additional([
+			'access_token' => $user->createToken($validated['device_name'])->plainTextToken,
+			'token_type'   => 'Bearer'
+		]);
 	}
 
 	/**
@@ -29,6 +39,6 @@ class AuthenticatedSessionController extends Controller {
 	public function destroy (): JsonResponse {
 		auth()->user()->tokens()->delete();
 
-		return response()->json('Has cerrado sesión satisfactoriamente');
+		return response()->json('Has cerrado sesión satisfactoriamente.');
 	}
 }
